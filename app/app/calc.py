255cb655eaa0ebca3e863a0e384f2a1a695d5e3c
dcrandell <david@onholdwizard.com>
2025-06-18 03:35:36 -0700

    added postgres adapter to dockerfile and requirements

---
+++ app/app/calc.py
@@ -0,0 +1,12 @@
+"""
+Caculator functions
+"""
+
+def add(x, y):
+    """ add x and y and return sum """
+    return x + y
+
+
+def subtract(x, y):
+    """ subtract x from y and return result """
+    return y - x
